2f5675f80dfb69d6d6eebfbc5894a5f1998d40eb
Haseeb ul Rehman <haseeb.rehman@ideofuzion.com>
2023-01-04 18:52:49 +0500

    fix: remove unused links from auth header

--- src/components/examples/auth/Header.tsx
+++ src/components/examples/auth/Header.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import { useRouter } from "next/router";
 import Image from "next/image";
 import useUser from "@/hooks/useUser";
 import fetchJson from "@/lib/fetchJson";
@@ -16,63 +15,50 @@ const linkStyle = css({
 
 export default function Header() {
   const { user, mutateUser } = useUser();
-  const router = useRouter();
+
+  const LoggedInLinks = () => (
+    <>
+      <li>
+        <Link className={linkStyle} href="/profile-sg">
+          <span
+            style={{
+              marginRight: ".3em",
+              verticalAlign: "middle",
+              borderRadius: "100%",
+              overflow: "hidden",
+            }}
+          >
+            <Image src={user?.avatarUrl || ""} width={32} height={32} alt="" />
+          </span>
+          Profile (Static Generation, recommended)
+        </Link>
+      </li>
+      <li>
+        <Link className={linkStyle} href="/profile-ssr">
+          Profile (Server-side Rendering)
+        </Link>
+      </li>
+      <li>
+        {/* In this case, we're fine with linking with a regular a in case of no JavaScript */}
+        {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
+        <a
+          className={linkStyle}
+          href="/api/logout"
+          onClick={async (e) => {
+            e.preventDefault();
+            mutateUser(await fetchJson("/api/logout", { method: "POST" }));
+          }}
+        >
+          Logout
+        </a>
+      </li>
+    </>
+  );
 
   return (
     <div>
       <ul>
-        <li>
-          <Link className={linkStyle} href="/">
-            Home
-          </Link>
-        </li>
-        {user?.isLoggedIn === false && (
-          <li>
-            <Link className={linkStyle} href="/login">
-              Login
-            </Link>
-          </li>
-        )}
-        {user?.isLoggedIn === true && (
-          <>
-            <li>
-              <Link className={linkStyle} href="/profile-sg">
-                <span
-                  style={{
-                    marginRight: ".3em",
-                    verticalAlign: "middle",
-                    borderRadius: "100%",
-                    overflow: "hidden",
-                  }}
-                >
-                  <Image src={user.avatarUrl} width={32} height={32} alt="" />
-                </span>
-                Profile (Static Generation, recommended)
-              </Link>
-            </li>
-            <li>
-              <Link className={linkStyle} href="/profile-ssr">
-                Profile (Server-side Rendering)
-              </Link>
-            </li>
-            <li>
-              {/* In this case, we're fine with linking with a regular a in case of no JavaScript */}
-              {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
-              <a
-                className={linkStyle}
-                href="/api/logout"
-                onClick={async (e) => {
-                  e.preventDefault();
-                  mutateUser(
-                    await fetchJson("/api/logout", { method: "POST" })
-                  );
-                }}
-              >
-                Logout
-              </a>
-            </li>
-          </>
-        )}
+        {user?.isLoggedIn === true && <LoggedInLinks />}
         <li>
           <a href="https://github.com/vvo/iron-session">
             <Image
@@ -88,9 +74,11 @@ export default function Header() {
         ul {
           display: flex;
           align-items: center;
+          justify-content: flex-end;
           list-style: none;
           margin-left: 0;
           padding-left: 0;
+          gap: 2rem;
         }
 
         li {
